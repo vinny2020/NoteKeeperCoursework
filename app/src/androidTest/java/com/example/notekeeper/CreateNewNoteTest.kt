91ef09eb4fc4ffce8ff2fc1d15ada50c2d32f395
Vincent Stoessel <vincents@backbase.com>
2020-06-19 09:03:23 -0400

    added instrumented UI test

---
+++ app/src/androidTest/java/com/example/notekeeper/CreateNewNoteTest.kt
@@ -0,0 +1,37 @@
+package com.example.notekeeper
+
+import androidx.test.espresso.Espresso.*
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.rule.ActivityTestRule
+import org.hamcrest.CoreMatchers.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class CreateNewNoteTest {
+    @Rule
+    @JvmField
+    val noteListActivity = ActivityTestRule(NoteListActivity::class.java)
+
+    @Test
+    fun createNewNote() {
+        val course = DataManager.courses["android_async"]
+        val noteTitle = "Test title note"
+        val noteText = "This is the body of our test note"
+
+        onView(withId(R.id.fab)).perform(click())
+
+        onView(withId(R.id.spinnerCourses)).perform(click())
+        onData(allOf(instanceOf(CourseInfo::class.java), equalTo(course))).perform(click())
+
+        onView(withId(R.id.textNoteTitle)).perform(typeText(noteTitle))
+        onView(withId(R.id.textNoteText)).perform(typeText(noteText))
+        pressBack()
+
+    }
+
+}
